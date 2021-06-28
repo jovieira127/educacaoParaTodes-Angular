9e285ea400523a6342e23a45b049c44167ccc89c
Barbara790 <barbara2.dossantos@gmail.com>
2021-06-28 10:11:58 -0300

    implementação do compomente menu lateral

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 
 
 
@@ -18,7 +19,8 @@ import { MatDividerModule } from '@angular/material/divider';
 @NgModule({
   declarations: [
     AppComponent,
-    TelaVisualizacaoComponent
+    TelaVisualizacaoComponent,
+    MenuLateralComponent
   ],
   imports: [
     BrowserModule,
