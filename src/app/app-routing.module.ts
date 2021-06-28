e43abaab577f56efa8d39de92abd1d7264ca236f
brunocallachi <brunocallachi@hotmail.com>
2021-06-28 09:54:26 -0300

    "implementação rodape, saibamais, equipe"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
+import { TimeComponent } from './time/time.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'rodape', component: RodapeComponent},
+  {path: 'saiba-mais', component: SaibaMaisComponent},
+  {path: 'time', component: TimeComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RodapeComponent } from './rodape/rodape.component';
+import { SaibaMaisComponent } from './saiba-mais/saiba-mais.component';
+import { TimeComponent } from './time/time.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RodapeComponent,
+    SaibaMaisComponent,
+    TimeComponent
   ],
   imports: [
     BrowserModule,
