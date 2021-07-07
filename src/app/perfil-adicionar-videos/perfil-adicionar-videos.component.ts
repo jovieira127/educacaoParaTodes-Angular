6938cddd34ca22263a8a39ff592cadd1c19a16ae
Barbara790 <barbara2.dossantos@gmail.com>
2021-07-07 19:32:13 -0300

    implementação do post em postagem

--- src/app/perfil-adicionar-videos/perfil-adicionar-videos.component.ts
+++ src/app/perfil-adicionar-videos/perfil-adicionar-videos.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
 
 
@@ -12,14 +15,20 @@ import { TemaService } from '../service/tema.service';
 })
 export class PerfilAdicionarVideosComponent implements OnInit {
 
+  postagem: Postagem = new Postagem() 
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
-  
+  idTema:number
+ 
+  user: Usuario = new Usuario()
+  idUser = environment.id
+
 
   constructor(
     private router: Router,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private postagemService: PostagemService,
   ) { }
 
   ngOnInit() {
@@ -28,6 +37,13 @@ export class PerfilAdicionarVideosComponent implements OnInit {
     }
     this.temaService.refreshToken()
     this.findAllTemas()
+   /*  this.getAllTemas() */
+  }
+
+  findByIdtema(){
+    this.temaService.getByIdTema(this.idTema).subscribe((resposta: Tema)=>{
+      this.tema = resposta
+    })
   }
 
   findAllTemas() {
@@ -35,6 +51,7 @@ export class PerfilAdicionarVideosComponent implements OnInit {
       this.listaTemas = resp
     })
   }
+
   
   cadastrar() {
     this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
@@ -44,4 +61,19 @@ export class PerfilAdicionarVideosComponent implements OnInit {
       this.tema = new Tema()
     })
   }
+
+  publicar(){
+    this.tema.id = this.idTema
+    this.postagem.tema = this.tema
+ 
+    this.user.id = this.idUser
+    this.postagem.usuario = this.user
+ 
+    this.postagemService.postPostagem(this.postagem).subscribe((resposta: Postagem)=>{
+      this.postagem = resposta
+      alert('Postagem realizada com sucesso!')
+      this.postagem = new Postagem()
+    })
+  }
+
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { Usuario } from '../model/Usuario';
 import { UsuarioLogin } from '../model/UsuarioLogin';
 
@@ -9,10 +10,19 @@ import { UsuarioLogin } from '../model/UsuarioLogin';
 })
 export class AuthService {
 
+
+
   constructor(
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
+  refreshToken(){
+    this.token = { headers: new HttpHeaders().set('Authorization',environment.token)}
+  }
 entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
   return this.http.post<UsuarioLogin>('http://localhost:8080/usuarios/login', usuarioLogin)
 }
