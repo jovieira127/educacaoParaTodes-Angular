a1def559ef2e238f3e88a54958e9c514b316e28a
PedroMagalhaes360 <henriquepedro13_@hotmail.com>
2021-07-08 21:20:28 -0300

    Erro de conflito no git

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,15 +28,9 @@ import { FeedComponent } from './feed/feed.component';
 import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-<<<<<<< HEAD
 import { MeusConteudosComponent } from './meus-conteudos/meus-conteudos.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
-=======
-import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
-import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
-
->>>>>>> origin/delete-put-postagem
 
 
 @NgModule({
@@ -54,12 +48,8 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
     PerfilVoluntarioComponent,
     PerfilEstudanteComponent,
     FeedComponent,   
-<<<<<<< HEAD
     PerfilAdicionarVideosComponent, TemaEditComponent, TemaDeleteComponent, MeusConteudosComponent, 
      PostagemEditComponent, PostagemDeleteComponent, 
-=======
-    PerfilAdicionarVideosComponent, TemaEditComponent, TemaDeleteComponent, PostagemEditComponent, PostagemDeleteComponent, 
->>>>>>> origin/delete-put-postagem
   ],
   imports: [
     BrowserModule,
