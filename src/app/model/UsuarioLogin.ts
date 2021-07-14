aa523b23f6637a13d82e3104a4287c0bc22dedc7
Barbara790 <barbara2.dossantos@gmail.com>
2021-07-14 09:40:01 -0300

    excluindo arquivos

--- src/app/model/UsuarioLogin.ts
+++
@@ -1,9 +0,0 @@
-export class UsuarioLogin{
-    public id: number
-    public nome: string
-    public senha: string
-    public email: string
-    public token: string
-    public foto: string
-    public tipo: string
-}
