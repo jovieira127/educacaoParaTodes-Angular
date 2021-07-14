e5272aee0e9881d8a5b78e035a9568da409f2587
Barbara790 <barbara2.dossantos@gmail.com>
2021-07-14 00:34:42 -0300

    atualizando todos os componentes

--- src/app/menu-superior/menu-superior.component.ts
+++ src/app/menu-superior/menu-superior.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment.prod';
+import { Usuario } from '../model/Usuario';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -8,12 +10,26 @@ import { AuthService } from '../service/auth.service';
 })
 export class MenuSuperiorComponent implements OnInit {
 
+usuario: Usuario= new Usuario()
+idUser= environment.id
+
+
   constructor(
+
     public authService: AuthService
   ) { }
 
   ngOnInit() {
-    
+
+    this.authService.refreshToken()
+
+    this.findByIdUser()
   }
 
+  findByIdUser() {
+    this.authService.refreshToken()
+    this.authService.getByIdUser(this.idUser).subscribe((resposta: Usuario) => {
+      this.usuario = resposta
+    })
+  }
 }
